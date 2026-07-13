f277da0bdfb283b36559645e1270b80fc5c6cabd
Jonny Noone <jonny.f.noone@gmail.com>
2026-07-13 21:30:28 +0100

    did the effect thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
   const [advice, setAdvice] = useState('');
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   async function getAdvice() {
+    console.log('getAdvice called...');
+
     const result = await fetch('https://api.adviceslip.com/advice');
     const data = await result.json();
 
