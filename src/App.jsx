0c177c566205b6c19be3e312ed74776b415a3f71
Jonny Noone <jonny.f.noone@gmail.com>
2026-07-13 21:02:16 +0100

    added advice count

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react';
 
 export default function App() {
   const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const result = await fetch('https://api.adviceslip.com/advice');
     const data = await result.json();
 
     setAdvice(data.slip.advice);
+    setCount(c => c + 1);
   }
 
   return (
     <>
       <h2>{advice}</h2>
       <button onClick={getAdvice}>Get Advice</button>
+      <p>You have read {count} pieces of advice.</p>
     </>
   );
 }
